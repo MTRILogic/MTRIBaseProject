23de2562d91fda89f42f4b096cf61173230e588a
Dean Samuel <deanprog@hotmail.com>
2025-09-28 01:30:38 -0600

    V0.0.4 Migrating from Groovy to KTS

---
+++ app/build.gradle.kts
@@ -0,0 +1,65 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.mtrilogic.mtrilogicproject"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 24
+
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    // ViewBinding
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    /*implementation(project(":fragmentable"))
+    implementation(project(":inflatable"))
+    implementation(project(":modelable"))
+    implementation(project(":recyclable"))*/
+
+    // 🔹 Usando repositorio local:
+    implementation("com.mtrilogic:modelable:0.0.0")
+    implementation("com.mtrilogic:fragmentable:0.0.0")
+    implementation("com.mtrilogic:inflatable:0.0.0")
+    implementation("com.mtrilogic:recyclable:0.0.0")
+
+    // 🔹 Alternativamente, usando JitPack (comentado si usas local)
+    // implementation("com.github.TU_USUARIO:MTRILogicLibraryProject:0.0.1")
+
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("com.google.android.material:material:1.13.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.compilerArgs.add("-Xlint:deprecation")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,18 +6,28 @@ pluginManagement {
     }
 }
 
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         mavenLocal()
         google()
         mavenCentral()
+
+        // Para usar tu repositorio local
+        maven {
+            url = uri("file:///D:/MTRI/LocalRepository")
+        }
+
+        // Para usar JitPack
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
 rootProject.name = "MTRILogicLibraryProject"
-include ':app'
-include ':modelable'
-include ':inflatable'
-include ':fragmentable'
-include ':recyclable'
+
+include(":app")
+include(":modelable")
+include(":inflatable")
+include(":fragmentable")
+include(":recyclable")
